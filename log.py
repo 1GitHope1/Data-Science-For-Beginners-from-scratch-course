5431871cec6ce25820e380808d61486ba6a0bd83
1GItHope1 <bybitura88@gmail.com>
2026-06-25 11:48:47 +0200

    Работа с файлом log.ipynb.
    Создал  файл log.ipynb (к нему автоматически создался py-file) и прописал в нем коротко те пункты , которые выполнил в ходе прохождения интро.

---
+++ log.py
@@ -0,0 +1,11 @@
+"""As of 25.06.2026 was completed as follows.
+
+-Downloaded the recommended Software form <INTRO>
+-Created a <conda env> only "SENATOROVAI", libs installed
+-Read linters documentation
+-Created all needed profiles on websites
+-filled & sent an internship contract
+-the repo for studying was forked then cloned on a local PC
+-connected to a team
+-created a discussion on GitHub (for maintaining reports)
+"""
